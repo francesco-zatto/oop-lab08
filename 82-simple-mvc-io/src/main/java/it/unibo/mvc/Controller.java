2412783dc78925534becfc04aa246e2a58ecb116
Francesco Zattoni <francesco.zattoni2@studio.unibo.it>
2022-11-28 10:10:21 +0100

    Implemented Controller, I have to write the ActionListener for the "Save" button

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,35 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    private static String HOME_FOLDER = System.getProperty("user.home");
+    private static String SEPARATOR = System.getProperty("file.separator");
+    private File currentFile = new File(HOME_FOLDER + SEPARATOR + "output.txt");
+
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
+
+    public void setCurrentFile(final File newFile) {
+        this.currentFile = Objects.requireNonNull(newFile);
+    }
+
+    public String getPath() {
+        return this.currentFile.getAbsolutePath();
+    }
+
+    public void writeOnCurrentFile(final String content) throws IOException {
+        final FileWriter fWriter = new FileWriter(this.currentFile);
+        fWriter.append(content);
+        fWriter.close();
+    }
 
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +17,45 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 3;
+    private static final String BUTTON_TEXT = "Save";
     private final JFrame frame = new JFrame();
+    private final JPanel panel;
+    private final JTextArea textArea;
+    private final JButton button;
+
+    public SimpleGUI() {
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.panel = new JPanel();
+        this.panel.setLayout(new BorderLayout());
+        this.textArea = new JTextArea();
+        this.button = new JButton(BUTTON_TEXT);
+        this.panel.add(this.textArea, BorderLayout.CENTER);
+        this.panel.add(this.button, BorderLayout.SOUTH);
+        this.frame.getContentPane().add(this.panel);
+        this.button.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                //TODO: add action when button pressed and Save the text in contextArea
+            }
+            
+        });
+    }
+
+    private void display() {
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        this.frame.setLocationByPlatform(true);
+        this.frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        final SimpleGUI myGUI = new SimpleGUI();
+        myGUI.display();
+
+    }
 
 }
